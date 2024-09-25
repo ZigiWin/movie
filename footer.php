1bbfcf1673526bec314b93db5ebede452932de10
Noob3mpire <teqa.teqa@mail.ru>
2024-09-25 14:58:12 +0300

    Создан footer

---
+++ footer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div style="background: black">
+    <footer>
+        <div class="container">
+            <div class="site-title">
+                <h3 style="font-size: 15px">Сайт концертно-экскурсионных программ</h3>
+            </div>
+            <div class="menu">
+                <h3>Мероприятия</h3>
+                <div class="events">
+                    <a href="#">Экскурсии</a>
+                    <a href="#">Концерты</a>
+                    <a href="#">Архив</a>
+                </div>
+            </div>
+            <div class="contacts">
+                <h3>Контакты</h3>
+                <p>г. Санкт-Петербург, Невский пр., д. 1</p>
+                <p>text@mail.ru</p>
+                <p>(812) 123-45-67</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Сайт концертно-экскурсионных программ</p>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
