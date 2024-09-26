cd0e245d5b6a785b675ec9c2227e04356849e2e1
Noob3mpire <teqa.teqa@mail.ru>
2024-09-26 14:45:03 +0300

    Добавлен хедер в afisha.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div class="container" style="align-items: center;">
+        <a class="header-title" href="#">САЙТ КОНЦЕРТНЫХ - ЭКСКУРСИОННЫХ ПРОГРАММ</a>
+        <nav>
+            <ul>
+                <li><a href="#">Афиша</a></li>
+                <li><a href="#">Заказ билетов</a></li>
+                <li><a href="#">Контакты</a></li>
+                <li><a href="#">История дворца</a></li>
+                <li><a href="#">Галерея</a></li>
+                <li><a href="#">Планы залов</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+</body>
+</html>
