1bf95daf19690ec792fa9ec772386b64a5518be5
Noob3mpire <teqa.teqa@mail.ru>
2024-09-26 14:44:41 +0300

    Добавлена страница афиша

---
+++ afisha.php
@@ -0,0 +1,71 @@
+<?php
+require_once 'header.php';?>
+<section class="afisha">
+    <h2 style="text-align: center">Афиша</h2>
+    <div class="posters">
+        <div class="poster">
+            <div class="date">
+                <span class="day">3</span>
+                <span class="month">июня</span>
+            </div>
+            <div class="image-placeholder"></div>
+            <div class="info">
+                <h2>«ВЕК имени СИНАТРЫ»</h2>
+                <p>Посвящение Фрэнку Синатре.</p>
+                <div class="details">
+                    <span class="type">Джаз</span>
+                    <span class="time">19:00</span>
+                    <span class="age">6+</span>
+                </div>
+            </div>
+            <button class="buy-ticket">Заказать билет</button>
+        </div>
+        <div class="poster">
+            <div class="date">
+                <span class="day">30</span>
+                <span class="month">июня</span>
+            </div>
+            <div class="image-placeholder"></div>
+            <div class="info">
+                <h2>Опера VS Оперетта</h2>
+                <p>Санкт-Петербургский камерный оркестр Olympic Orchestra</p>
+                <div class="details">
+                    <span class="type">Классика</span>
+                </div>
+            </div>
+            <button class="buy-ticket">Заказать билет</button>
+        </div>
+        <div class="poster">
+            <div class="date">
+                <span class="day">30</span>
+                <span class="month">июня</span>
+            </div>
+            <div class="image-placeholder"></div>
+            <div class="info">
+                <h2>Опера VS Оперетта</h2>
+                <p>Санкт-Петербургский камерный оркестр Olympic Orchestra</p>
+                <div class="details">
+                    <span class="type">Классика</span>
+                </div>
+            </div>
+            <button class="buy-ticket">Заказать билет</button>
+        </div>
+        <div class="poster">
+            <div class="date">
+                <span class="day">30</span>
+                <span class="month">июня</span>
+            </div>
+            <div class="image-placeholder"></div>
+            <div class="info">
+                <h2>Опера VS Оперетта</h2>
+                <p>Санкт-Петербургский камерный оркестр Olympic Orchestra</p>
+                <div class="details">
+                    <span class="type">Классика</span>
+                </div>
+            </div>
+            <button class="buy-ticket">Заказать билет</button>
+        </div>
+    </div>
+</section>
+<?php
+require_once 'footer.php';?>
