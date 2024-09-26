5a16e1b6339bd02eb258155bd91c1ee93b1175d8
Noob3mpire <teqa.teqa@mail.ru>
2024-09-26 14:27:08 +0300

    Добавлен poster для афиши

---
+++ poster.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="poster">
+    <div class="date">
+        <span class="day">3</span>
+        <span class="month">июня</span>
+    </div>
+    <div class="info">
+        <h2>«ВЕК имени СИНАТРЫ»</h2>
+        <p>Посвящение Фрэнку Синатре.</p>
+        <div class="details">
+            <span class="time">19:00</span>
+            <span class="age">6+</span>
+        </div>
+        <button class="buy-ticket">Заказать билет</button>
+    </div>
+    <div class="image-placeholder"></div>
+</div>
+</body>
+</html>
